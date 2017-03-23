6f9ff0850a52fa55c12640f8d7aea9d66f03f00a
techmashido <jaikumar19126@gmail.com>
2017-03-23 13:24:42 +0000

    housekeeping

--- aosp_ham.mk
+++ aosp_ham.mk
@@ -16,9 +16,6 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-# $(call inherit-product, vendor/pure/configs/pure_phone.mk)
-
 # Inherit from the common Open Source product configuration
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
